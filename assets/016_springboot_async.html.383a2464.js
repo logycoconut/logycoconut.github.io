4c1dccbd6f1568b6fc657692dcb0244c81679da8
logycoconut <logycoconut@users.noreply.github.com>
2022-12-12 16:49:18 +0000

    deploy: 960abeacbb27b0e0be1fdfade1e547acd68fa2d2

---
+++ assets/016_springboot_async.html.383a2464.js
@@ -0,0 +1,51 @@
+import{_ as l}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as d,c as s,a as e,b as a,d as c,e as n,r}from"./app.296972cb.js";const v={},o=n(`<blockquote><p>Spring的定时任务默认是单线程串行执行的, 那假设第一个任务比较耗时长, 直到超过任务二的设定时间之后才完成, 那么就会造成任务二不能及时完成, 进而造成其他问题</p></blockquote><h2 id="先来说说定时任务" tabindex="-1"><a class="header-anchor" href="#先来说说定时任务" aria-hidden="true">#</a> 先来说说定时任务</h2><p>开启对定时任务的支持, 在启动类上加上<code>@EnableScheduling</code></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>// 启动类
+@EnableScheduling
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+// 定时任务类
+@Slf4j
+@Service
+public class ScheduledService {
+    @Scheduled(fixedRate = 5000)
+    public void scheduled1() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+
+    @Scheduled(cron = &quot;0/5 * * * * *&quot;)
+    public void scheduled2() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>运行可以看到定时任务都已经执行，并且使同一个线程中串行执行</p><p><img src="https://i.loli.net/2020/08/20/36FeR7hJVqcsmlf.png" alt="image.png" loading="lazy"></p><p><em>注 : cron表达式的配置非常灵活, 但是刚接触的话会产生困惑, 可以用工具生成</em></p>`,7),u={href:"https://www.bejson.com/othertools/cron/",target:"_blank",rel:"noopener noreferrer"},t=n(`<h2 id="开启异步支持" tabindex="-1"><a class="header-anchor" href="#开启异步支持" aria-hidden="true">#</a> 开启异步支持</h2><p><code>@EnableAsync</code>开启异步支持, 并在方法上加上<code>@Async</code>注解</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>// 启动类
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+// 定时任务类
+@Slf4j
+@Service
+public class ScheduledService {
+
+    @Async
+    @Scheduled(fixedRate = 5000)
+    public void scheduled1() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+
+    @Async
+    @Scheduled(cron = &quot;0/5 * * * * *&quot;)
+    public void scheduled2() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>重启项目，会发现每一个任务都是在不同的线程中运行</p><p><img src="https://i.loli.net/2020/08/20/IUnLSZ7xW5hN3qJ.png" alt="image.png" loading="lazy"></p>`,5);function m(b,p){const i=r("ExternalLinkIcon");return d(),s("div",null,[o,e("p",null,[e("a",u,[a("在线生成cron表达式"),c(i)])]),t])}const _=l(v,[["render",m],["__file","016_springboot_async.html.vue"]]);export{_ as default};
---
+++ assets/20221210_有时间要多陪陪父母.html.e38ed69c.js
@@ -0,0 +1,15 @@
+import{_ as d}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as r,c as o,a as e,b as n,d as t,e as l,r as s}from"./app.296972cb.js";const c={},u=e("p",null,"今天在 V2EX 上无意间刷到一个评论",-1),p=e("div",{class:"language-text line-numbers-mode","data-ext":"text"},[e("pre",{class:"language-text"},[e("code",null,`" You've already spent more than 90% of your time with your parents. "
+`)]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"})])],-1),v={href:"https://www.youtube.com/watch?v=JXeJANDKwDc&t=331s",target:"_blank",rel:"noopener noreferrer"},_=l(`<p>里面有一段内容讨论了人一生陪伴父母的时间</p><p>我在这里简单描述一下 UP 的观点</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>假定人的寿命为 100 岁，也就是 5200 周（一年为52周）
+
+在我们人生的前二十几年，我们几乎天天和父母在一起 
+
+在我们外出求学的时光中，每年只有长假期才能回家（例如七天假、寒暑假），一年中只有四分之三的时间和家人呆在一起
+
+毕业后，大部分人为了工作会在另一个城市独自生活，如果我们努力与父母在一起整整两周，那我们还是花费了超过90%的时间和他们在一起
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>UP 提供的公式为</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>( 前20年和父母相处的时间 + 独立生活后和父母相处的时间 ) / 总共和父母相处的时间
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>我对这个公式做一些些的调整</p><p><strong>我们和父母相处时间占比有多个变量共同决定</strong></p><ul><li>外出求学的时间点</li></ul><p>在国内，大学生活一般是从19岁开始</p><ul><li>已经工作的时间</li></ul><p>工作之后，陪家人的时间就会愈发的少</p><ul><li>一年中可以陪伴父母的天数</li></ul><p>以一个普通的打工人沃兹基为例</p>`,13),m={href:"http://www.gov.cn/zhengce/content/2021-10/25/content_5644835.htm",target:"_blank",rel:"noopener noreferrer"},g=e("strong",null,"31天",-1),b=l(`<p>没有意外的话，一年当中他和父母在一起的时间为20天 （春节7天 + 端午3天 + 中秋 3天 + 东拼西凑的7天 ）</p><p>所以，符合他的最终公式为</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>已经陪伴的时间 X：18  * 52 周 + 3 * 13 周 + 4 * 3周
+
+理论剩余陪伴时间 Y：（100 - 27） * 3周
+
+和父母的相处时间占比 = X / (X + Y)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>最后附上一张 iOS 自带的日历图，大家看下自己在一年和父母在一起的日子吧 <img src="https://knowledge-img-1304942245.cos.ap-shanghai.myqcloud.com/IMG_3530.jpeg" alt="" loading="lazy"></p>`,4);function h(x,f){const i=s("ExternalLinkIcon"),a=s("YouTube");return r(),o("div",null,[u,p,e("p",null,[n("这句话出自 Youtube 上的一个视频 "),e("a",v,[n("What Are You Doing With Your Life? The Tail End"),t(i)])]),t(a,{id:"JXeJANDKwDc"}),_,e("p",null,[n("根据 "),e("a",m,[n("国务院办公厅关于2022年部分节假日安排的通知"),t(i)]),n("， 可以算出他的法定假期长度总共为"),g,n("， 但是类似的像「元旦 + 春节」、「中秋 + 国庆」这类贴近的假期，他会选择元旦不回家，中秋或者国庆不回家，有些假期可能会选择和朋友、同学一起出去玩")]),b])}const k=d(c,[["render",h],["__file","20221210_有时间要多陪陪父母.html.vue"]]);export{k as default};

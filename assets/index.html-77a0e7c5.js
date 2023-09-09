e5f2c54071f30b9acfc10944779cc9ef114bb19c
logycoconut <logycoconut@users.noreply.github.com>
2023-09-09 04:38:52 +0000

    deploy: 95527afffd7172215a8df8cfb2b4aaaa235c527e

--- assets/index.html-77a0e7c5.js
+++
@@ -1,1 +0,0 @@
-import{_ as l}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as t,a as e}from"./app-458147e9.js";const o={},c=e("ul",null,[e("li",null,[e("p",null,"在这个赛场上，努力是最不值得拿出来夸耀的东西，因为这只是基本，是人人都能做到的，是最底层、最渺小的东西。"),e("p",null,"—— 全职高手")])],-1),_=[c];function s(r,a){return n(),t("div",null,_)}const d=l(o,[["render",s],["__file","index.html.vue"]]);export{d as default};

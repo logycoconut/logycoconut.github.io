89df1c8acb3c0ff2db3b770c4602e2b0e0eb2a45
logycoconut <logycoconut@users.noreply.github.com>
2023-10-29 16:45:56 +0000

    deploy: dcf7aa78f5e64f2891be46a525887b03c1765a40

---
+++ assets/200721_SpringMVC 参数验证中 BindingResult 的使用.html-73f1f3c5.js
@@ -0,0 +1,28 @@
+import{_ as i}from"./plugin-vue_export-helper-c27b6911.js";import{o as e,c as n,f as a}from"./app-b907f58c.js";const r={},d=a(`<blockquote><p>在不需要统一处理异常时使用</p></blockquote><h2 id="代码示例" tabindex="-1"><a class="header-anchor" href="#代码示例" aria-hidden="true">#</a> 代码示例</h2><h3 id="创建实体类" tabindex="-1"><a class="header-anchor" href="#创建实体类" aria-hidden="true">#</a> 创建实体类</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>@Data
+public class User {
+    @NotNull(message = &quot;用户名不能为空&quot;)
+    private String name;
+    @NotEmpty(message = &quot;密码不能是空值&quot;)
+    private String password;
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="编写web层代码" tabindex="-1"><a class="header-anchor" href="#编写web层代码" aria-hidden="true">#</a> 编写WEB层代码</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>@Slf4j
+@RestController
+@RequestMapping(&quot;user&quot;)
+public class UserController {
+
+    @GetMapping(&quot;login1&quot;)
+    public String login1(@Valid User user, BindingResult result) {
+        if (result.hasErrors()) {
+            result.getAllErrors()
+                    .forEach(error -&gt; log.info(error.getDefaultMessage()));
+        }
+        return &quot;登录成功&quot;;
+    }
+
+    @GetMapping(&quot;login2&quot;)
+    public String login2(@Valid User user) {
+        return &quot;登录成功&quot;;
+    }
+    
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="结果对比" tabindex="-1"><a class="header-anchor" href="#结果对比" aria-hidden="true">#</a> 结果对比</h2><h3 id="_1-参数中有bindingresult" tabindex="-1"><a class="header-anchor" href="#_1-参数中有bindingresult" aria-hidden="true">#</a> ① 参数中有BindingResult</h3><h4 id="返回结果" tabindex="-1"><a class="header-anchor" href="#返回结果" aria-hidden="true">#</a> 返回结果：</h4><figure><img src="https://i.loli.net/2020/07/22/yJE6iKFXaMrlwkp.png" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure><h4 id="控制台信息" tabindex="-1"><a class="header-anchor" href="#控制台信息" aria-hidden="true">#</a> 控制台信息：</h4><figure><img src="https://i.loli.net/2020/07/22/tGa2AP9plj8zhmM.png" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure><p>异常并没有被抛出，在 web 层中获取了参数验证错误信息并在控制台打印了出来</p><hr><h3 id="_2-参数中没有bindingresult" tabindex="-1"><a class="header-anchor" href="#_2-参数中没有bindingresult" aria-hidden="true">#</a> ② 参数中没有BindingResult</h3><h4 id="返回结果-1" tabindex="-1"><a class="header-anchor" href="#返回结果-1" aria-hidden="true">#</a> 返回结果：</h4><figure><img src="https://i.loli.net/2020/07/22/jtQ5w7zX1vLVRlZ.png" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure><h4 id="控制台信息-1" tabindex="-1"><a class="header-anchor" href="#控制台信息-1" aria-hidden="true">#</a> 控制台信息：</h4><figure><img src="https://i.loli.net/2020/07/22/NJ1xmvYwZXDIioM.png" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure><p>抛出参数校验异常，框架为我们返回了一段信息，平时我们可以通过捕获全局异常的方式将校验异常包装到我们自定义的异常中，取出错误信息并与错误代码一起返回</p>`,20),s=[d];function l(t,u){return e(),n("div",null,s)}const h=i(r,[["render",l],["__file","200721_SpringMVC 参数验证中 BindingResult 的使用.html.vue"]]);export{h as default};
---
+++ assets/200818_SpringBoot 中定时任务的异步执行.html-a5459f08.js
@@ -0,0 +1,51 @@
+import{_ as l}from"./plugin-vue_export-helper-c27b6911.js";import{r as d,o as s,c as a,a as e,b as c,d as r,f as i}from"./app-b907f58c.js";const v={},o=i(`<blockquote><p>Spring 的定时任务默认是单线程串行执行的, 那假设第一个任务比较耗时长, 直到超过任务二的设定时间之后才完成, 那么就会造成任务二不能及时完成, 进而造成其他问题</p></blockquote><h2 id="先来说说定时任务" tabindex="-1"><a class="header-anchor" href="#先来说说定时任务" aria-hidden="true">#</a> 先来说说定时任务</h2><p>开启对定时任务的支持, 在启动类上加上<code>@EnableScheduling</code></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>// 启动类
+@EnableScheduling
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+// 定时任务类
+@Slf4j
+@Service
+public class ScheduledService {
+    @Scheduled(fixedRate = 5000)
+    public void scheduled1() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+
+    @Scheduled(cron = &quot;0/5 * * * * *&quot;)
+    public void scheduled2() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>运行可以看到定时任务都已经执行，并且使同一个线程中串行执行</p><figure><img src="https://i.loli.net/2020/08/20/36FeR7hJVqcsmlf.png" alt="image.png" tabindex="0" loading="lazy"><figcaption>image.png</figcaption></figure><p><em>注 : cron表达式的配置非常灵活, 但是刚接触的话会产生困惑, 可以用工具生成</em></p>`,7),t={href:"https://www.bejson.com/othertools/cron/",target:"_blank",rel:"noopener noreferrer"},u=i(`<h2 id="开启异步支持" tabindex="-1"><a class="header-anchor" href="#开启异步支持" aria-hidden="true">#</a> 开启异步支持</h2><p><code>@EnableAsync</code>开启异步支持, 并在方法上加上<code>@Async</code>注解</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>// 启动类
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+// 定时任务类
+@Slf4j
+@Service
+public class ScheduledService {
+
+    @Async
+    @Scheduled(fixedRate = 5000)
+    public void scheduled1() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+
+    @Async
+    @Scheduled(cron = &quot;0/5 * * * * *&quot;)
+    public void scheduled2() {
+        log.info(&quot;----{}----&quot;, Thread.currentThread().getName());
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>重启项目，会发现每一个任务都是在不同的线程中运行</p><figure><img src="https://i.loli.net/2020/08/20/IUnLSZ7xW5hN3qJ.png" alt="image.png" tabindex="0" loading="lazy"><figcaption>image.png</figcaption></figure>`,5);function m(b,p){const n=d("ExternalLinkIcon");return s(),a("div",null,[o,e("p",null,[e("a",t,[c("在线生成cron表达式"),r(n)])]),u])}const _=l(v,[["render",m],["__file","200818_SpringBoot 中定时任务的异步执行.html.vue"]]);export{_ as default};

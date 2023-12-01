975113eeca15aa8d4f3efcba85b0e49e9b36517f
logycoconut <logycoconut@users.noreply.github.com>
2023-12-01 07:39:26 +0000

    deploy: 15698a73a2615571ecf641c671db9a1c471cd772

--- assets/Node 版本升级.html-2955fbb3.js
+++
@@ -1,9 +0,0 @@
-import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{o as s,c as e,f as a}from"./app-c91d02ba.js";const c={},o=a(`<div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code> <span class="token comment"># 查看当前 node 版本 </span>
- <span class="token function">node</span> <span class="token parameter variable">-v</span>
- <span class="token comment"># 全局安装 n </span>
- <span class="token function">sudo</span> <span class="token function">npm</span> <span class="token function">install</span> n <span class="token parameter variable">-g</span>
- <span class="token comment"># 查看 node 版本</span>
- <span class="token function">npm</span> view <span class="token function">node</span> versions
- <span class="token comment"># 安装指定版本（或最新稳定版</span>
- <span class="token function">sudo</span> n stable
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),i=[o];function t(l,d){return s(),e("div",null,i)}const m=n(c,[["render",t],["__file","Node 版本升级.html.vue"]]);export{m as default};
---
+++ assets/Node 版本升级.html-3ba7362c.js
@@ -0,0 +1,9 @@
+import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{o as s,c as e,f as a}from"./app-358b0ff8.js";const o={},c=a(`<h3 id="使用-n-包升级-node" tabindex="-1"><a class="header-anchor" href="#使用-n-包升级-node" aria-hidden="true">#</a> 使用 <code>n</code> 包升级 <code>node</code></h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code> <span class="token comment"># 查看当前 node 版本 </span>
+ <span class="token function">node</span> <span class="token parameter variable">-v</span>
+ <span class="token comment"># 全局安装 n </span>
+ <span class="token function">sudo</span> <span class="token function">npm</span> <span class="token function">install</span> n <span class="token parameter variable">-g</span>
+ <span class="token comment"># 查看 node 版本</span>
+ <span class="token function">npm</span> view <span class="token function">node</span> versions
+ <span class="token comment"># 安装指定版本（或最新稳定版</span>
+ <span class="token function">sudo</span> n stable
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),i=[c];function t(d,l){return s(),e("div",null,i)}const m=n(o,[["render",t],["__file","Node 版本升级.html.vue"]]);export{m as default};
